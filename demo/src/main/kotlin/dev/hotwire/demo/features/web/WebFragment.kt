f8742be8f02063b54d8b5105332a8d233135cf04
Noah Godel <ng@oxon.ch>
2025-10-14 18:27:27 +0200

    fix: quick fix for js tabs refresh issue

--- demo/src/main/kotlin/dev/hotwire/demo/features/web/WebFragment.kt
+++ demo/src/main/kotlin/dev/hotwire/demo/features/web/WebFragment.kt
@@ -7,15 +7,33 @@ import dev.hotwire.core.turbo.errors.HttpError
 import dev.hotwire.core.turbo.errors.VisitError
 import dev.hotwire.core.turbo.visit.VisitAction.REPLACE
 import dev.hotwire.core.turbo.visit.VisitOptions
+import dev.hotwire.core.turbo.webview.HotwireWebView
 import dev.hotwire.demo.R
 import dev.hotwire.navigation.destinations.HotwireDestinationDeepLink
 import dev.hotwire.navigation.fragments.HotwireWebFragment
+import dev.hotwire.navigation.session.SessionModalResult
 
 @HotwireDestinationDeepLink(uri = "hotwire://fragment/web")
 open class WebFragment : HotwireWebFragment() {
+    private var startedAfterModalResult = false
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupMenu()
+    }
+
+    override fun onWebViewDetached(webView: HotwireWebView) {
+        if (!this.startedAfterModalResult) {
+            // in the framework code there are extentions to get the value -> arguments?.location
+            arguments?.putString("location", webView.url)
+        }
+        this.startedAfterModalResult = false
+        super.onWebViewDetached(webView)
+    }
+
+    override fun onStartAfterModalResult(result: SessionModalResult) {
+        this.startedAfterModalResult = true
+        super.onStartAfterModalResult(result)
     }
 
     override fun onFormSubmissionStarted(location: String) {
